997e58cbf614ab4400b2c85898da6e648b837ad4
Diego_93 <dievazqu@itba.edu.ar>
2015-09-10 20:03:56 -0300

    database connection singleton

--- src/main/java/ar/edu/itba/it/paw/db/Database.java
+++ src/main/java/ar/edu/itba/it/paw/db/Database.java
@@ -14,45 +14,50 @@ import java.util.Properties;
 
 public abstract class Database<T> {
 
-	protected Connection mDbConnection;
+	private static Connection mDbConnection = null;
 
-	protected Database() {
-
-		URI dbUri;
-		try {
-			//dbUri = new URI("postgres://qycdoftytvgugn:3SS5BQFZsq1FKJXrMLSJtX5fat@ec2-54-83-55-214.compute-1.amazonaws.com:5432/djnrag8vu0hpu"); 
-			dbUri = new URI(
-					"postgres://qycdoftytvgugn:3SS5BQFZsq1FKJXrMLSJtX5fat@localhost:3000/djnrag8vu0hpu");
-			String username = dbUri.getUserInfo().split(":")[0];
-			String password = dbUri.getUserInfo().split(":")[1];
-			String dbUrl = "jdbc:postgresql://" + dbUri.getHost() + ':' + dbUri.getPort() + dbUri.getPath();
-			String dbDriver = "org.postgresql.Driver";
-			Properties prop = new Properties();
-			prop.setProperty("user", username);
-			prop.setProperty("password", password);
-			String ssloff = "?ssl=true&sslfactory=org.postgresql.ssl.NonValidatingFactory";
-
-			/*
-			 * String dbUrl = "jdbc:postgresql://localhost/paw";
-			 * 
-			 * String userName = "postgres"; String password = "paw";
-			 */
-
+	private static void init(){
+		if(mDbConnection==null){
+			URI dbUri;
 			try {
-				Class.forName(dbDriver);
-				dbUrl+=ssloff;
-				mDbConnection = DriverManager.getConnection(dbUrl, prop);
-				//mDbConnection = DriverManager.getConnection(dbUrl, username, password);
-				System.out.println("Got Connection");
-			} catch (ClassNotFoundException e) {
-				e.printStackTrace();
-			} catch (SQLException e) {
-				e.printStackTrace();
+				//dbUri = new URI("postgres://qycdoftytvgugn:3SS5BQFZsq1FKJXrMLSJtX5fat@ec2-54-83-55-214.compute-1.amazonaws.com:5432/djnrag8vu0hpu"); 
+				dbUri = new URI(
+						"postgres://qycdoftytvgugn:3SS5BQFZsq1FKJXrMLSJtX5fat@localhost:3000/djnrag8vu0hpu");
+				String username = dbUri.getUserInfo().split(":")[0];
+				String password = dbUri.getUserInfo().split(":")[1];
+				String dbUrl = "jdbc:postgresql://" + dbUri.getHost() + ':' + dbUri.getPort() + dbUri.getPath();
+				String dbDriver = "org.postgresql.Driver";
+				Properties prop = new Properties();
+				prop.setProperty("user", username);
+				prop.setProperty("password", password);
+				String ssloff = "?ssl=true&sslfactory=org.postgresql.ssl.NonValidatingFactory";
+
+				/*
+				 * String dbUrl = "jdbc:postgresql://localhost/paw";
+				 * 
+				 * String userName = "postgres"; String password = "paw";
+				 */
+
+				try {
+					Class.forName(dbDriver);
+					dbUrl+=ssloff;
+					mDbConnection = DriverManager.getConnection(dbUrl, prop);
+					//mDbConnection = DriverManager.getConnection(dbUrl, username, password);
+					System.out.println("Got Connection");
+				} catch (ClassNotFoundException e) {
+					e.printStackTrace();
+				} catch (SQLException e) {
+					e.printStackTrace();
+				}
+			} catch (URISyntaxException e1) {
+				System.out.println("No se pudo conectar con la base de datos");
+				e1.printStackTrace();
 			}
-		} catch (URISyntaxException e1) {
-			System.out.println("No se pudo conectar con la base de datos");
-			e1.printStackTrace();
 		}
+	}
+	
+	protected Database() {
+		init();
 	}
 	
 	protected T doQuery(String sql) {
