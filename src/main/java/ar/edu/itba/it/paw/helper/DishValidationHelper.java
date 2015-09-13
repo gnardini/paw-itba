746b950bf609e8317aef7796d4850383b73692a2
gnardini <gonzalo.nardini@wolox.com.ar>
2015-09-13 18:49:24 -0300

    Fix: new dishes

--- src/main/java/ar/edu/itba/it/paw/helper/DishValidationHelper.java
+++ src/main/java/ar/edu/itba/it/paw/helper/DishValidationHelper.java
@@ -27,7 +27,7 @@ public class DishValidationHelper {
 				|| menu == "")
 			return false;
 		Type type = getType(menu);
-		mDish = new Dish(Integer.valueOf(mRequest.getParameter("code")), name, description, Integer.valueOf(price), type);
+		mDish = new Dish(Long.valueOf(mRequest.getParameter("restaurantId")), name, description, Integer.valueOf(price), type);
 		return true;
 	}
 	
