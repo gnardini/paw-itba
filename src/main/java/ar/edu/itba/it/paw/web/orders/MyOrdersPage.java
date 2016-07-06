0087b4e69baed3800e28ad13bf64e6d29002a10f
Diego_93 <dievazqu@itba.edu.ar>
2016-07-06 18:46:40 -0300

    show minimum info about orders

--- src/main/java/ar/edu/itba/it/paw/web/orders/MyOrdersPage.java
+++ src/main/java/ar/edu/itba/it/paw/web/orders/MyOrdersPage.java
@@ -1,15 +1,31 @@
 package ar.edu.itba.it.paw.web.orders;
 
+import org.apache.wicket.markup.html.WebMarkupContainer;
 import org.apache.wicket.markup.html.basic.Label;
-import org.apache.wicket.spring.injection.annot.SpringBean;
+import org.apache.wicket.markup.html.list.ListItem;
+import org.apache.wicket.markup.html.list.ListView;
+import org.springframework.core.annotation.Order;
 
-import ar.edu.itba.it.paw.repository.OrderRepo;
+import ar.edu.itba.it.paw.model.Orders;
 import ar.edu.itba.it.paw.web.base.BasePage;
 
 public class MyOrdersPage extends BasePage {
 
-	
-	public MyOrdersPage(){
+	public MyOrdersPage() {
 		add(new Label("pageName", "Mis Pedidos"));
+		ListView<Orders> orderListView = new ListView<Orders>("orderList", loggedUser.getOrders()) {
+			@Override
+			protected void populateItem(ListItem<Orders> item) {
+				Orders order = item.getModelObject();
+				item.add(new Label("restaurantName", order.getRestaurant().getName()));
+				item.add(new Label("price", String.format("%.02f", order.getPrice())));
+				item.add(new Label("orderDate", order.getOrderDate()));
+				item.add(new Label("delivered", (order.isDelivered() ? "Entregado" : "No entregado")));
+			/*	WebMarkupContainer deliveredButtonContainer = new WebMarkupContainer("deliveredButtonContainer");
+				
+				item.add(deliveredButtonContainer);*/
+			}
+		};
+		add(orderListView);
 	}
 }
